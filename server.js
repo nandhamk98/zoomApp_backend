170fadcac5cc5dad375576ebb0649dad58433dc1
M k <namk@informatica.com>
2022-07-02 16:59:16 +0530

    Added DB functionality

--- server.js
+++ server.js
@@ -1,21 +1,83 @@
-const express = require("express");
-const http = require("http");
-const { v4: uuidv4 } = require("uuid");
-const cors = require("cors");
-const twilio = require("twilio");
+// const express = require("express");
+// const http = require("http");
+// const { v4: uuidv4 } = require("uuid");
+// const cors = require("cors");
+// const twilio = require("twilio");
+// const {MongoClient} = require("mongodb"); 
+import express  from 'express';
+import http from 'http';
+import {v4 as uuidv4} from 'uuid';
+import cors from 'cors';
+import twilio from 'twilio';
+import {MongoClient} from 'mongodb';
+import { Server } from "socket.io";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5002;
 const app = express();
 const server = http.createServer(app);
 
+// const url = ''
+const url = process.env.MONGO_URL;
+
+
+const createConnection = async ()=>{
+  const client = new MongoClient(url);
+  await client.connect();
+  console.log("connected to db")
+  return client;
+}
+
+const client = await createConnection();
+
+
+
+const getRoomData = async () => {
+  const data = await client.db("Zoom").collection('Room').find({}).toArray();
+  return data;
+}
+
+const insertRoomData = async (data) => {
+  const response = await client.db("Zoom").collection('Room').insertOne(data);
+  return response;
+}
+
+const updateRoomData = async (filteredParam, Data) => {
+  const response = await client.db("Zoom").collection('Room').updateOne(filteredParam,{$set :Data} );
+  return response;
+}
+
+
+const getUserData = async () => {
+  const data = await client.db("Zoom").collection('AppUser').find({}).toArray();
+  return data
+}
+
+const insertUserData = async (data) => {
+  const response = await client.db("Zoom").collection('AppUser').insertOne(data);
+  return response;
+}
+
+const removeUserData = async (data) => {
+  const response = await client.db("Zoom").collection('AppUser').deleteOne(data);
+  return response;
+}
+
+
+const removeRoomData = async (data) => {
+  const response = await client.db("Zoom").collection('Room').deleteOne(data);
+}
 app.use(cors());
 
-let connectedUsers = [];
-let rooms = [];
+// let connectedUsers = [];
+// let rooms = [];
 
 // create route to check if room exists
-app.get("/api/room-exists/:roomId", (req, res) => {
+app.get("/api/room-exists/:roomId", async (req, res) => {
   const { roomId } = req.params;
+  const rooms = await getRoomData();
   const room = rooms.find((room) => room.id === roomId);
 
   if (room) {
@@ -32,8 +94,8 @@ app.get("/api/room-exists/:roomId", (req, res) => {
 });
 
 app.get("/api/get-turn-credentials", (req, res) => {
-  const accountSid = "ACfcd0fb8c119d4abf46d53e6deb581472";
-  const authToken = "ad2ebd7a7fb272518682374bc26db605";
+  const accountSid = process.env.ACCOUNT_SECRET_KEY
+  const authToken = process.env.AUTH_TOKEN;
 
   const client = twilio(accountSid, authToken);
 
@@ -49,7 +111,7 @@ app.get("/api/get-turn-credentials", (req, res) => {
   }
 });
 
-const io = require("socket.io")(server, {
+const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
@@ -86,9 +148,10 @@ io.on("connection", (socket) => {
 
 // socket.io handlers
 
-const createNewRoomHandler = (data, socket) => {
+const createNewRoomHandler = async (data, socket) => {
+  const rooms = await getRoomData();
   console.log("host is creating new room");
-  console.log(data);
+  // console.log(data);
   const { identity, onlyAudio } = data;
 
   const roomId = uuidv4();
@@ -103,8 +166,9 @@ const createNewRoomHandler = (data, socket) => {
   };
 
   // push that user to connectedUsers
-  connectedUsers = [...connectedUsers, newUser];
+  let connectedUsers = [ newUser];
 
+  await insertUserData(newUser);
   //create new room
   const newRoom = {
     id: roomId,
@@ -113,17 +177,19 @@ const createNewRoomHandler = (data, socket) => {
   // join socket.io room
   socket.join(roomId);
 
-  rooms = [...rooms, newRoom];
+  const roomInsert = await insertRoomData(newRoom);
 
   // emit to that client which created that room roomId
   socket.emit("room-id", { roomId });
+  // console.log(rooms);
+  // console.log(connectedUsers)
 
   // emit an event to all users connected
   // to that room about new users which are right in this room
   socket.emit("room-update", { connectedUsers: newRoom.connectedUsers });
 };
 
-const joinRoomHandler = (data, socket) => {
+const joinRoomHandler = async (data, socket) => {
   const { identity, roomId, onlyAudio } = data;
 
   const newUser = {
@@ -135,55 +201,73 @@ const joinRoomHandler = (data, socket) => {
   };
 
   // join room as user which just is trying to join room passing room id
+  const rooms = await getRoomData();
+  await insertUserData(newUser);
   const room = rooms.find((room) => room.id === roomId);
-  room.connectedUsers = [...room.connectedUsers, newUser];
+  if(room){
+    let connectedUsers = [...room.connectedUsers, newUser]
 
-  // join socket.io room
-  socket.join(roomId);
+    let updateConnectUser ={
+      connectedUsers }
+    await updateRoomData({id: room.id},updateConnectUser)
 
-  // add new user to connected users array
-  connectedUsers = [...connectedUsers, newUser];
+    // join socket.io room
+    socket.join(roomId);
 
-  // emit to all users which are already in this room to prepare peer connection
-  room.connectedUsers.forEach((user) => {
-    if (user.socketId !== socket.id) {
-      const data = {
-        connUserSocketId: socket.id,
-      };
+    // add new user to connected users array
+    connectedUsers = [...connectedUsers, newUser];
+  
+    // emit to all users which are already in this room to prepare peer connection
+    connectedUsers.forEach((user) => {
+      if (user.socketId !== socket.id) {
+        const data = {
+          connUserSocketId: socket.id,
+        };
+  
+        io.to(user.socketId).emit("conn-prepare", data);
+      }
+    });
+  
+    io.to(roomId).emit("room-update", { connectedUsers: room.connectedUsers });
+  }
+  
 
-      io.to(user.socketId).emit("conn-prepare", data);
-    }
-  });
 
-  io.to(roomId).emit("room-update", { connectedUsers: room.connectedUsers });
+ 
 };
 
-const disconnectHandler = (socket) => {
+const disconnectHandler = async (socket) => {
   // find if user has been registered - if yes remove him from room and connected users array
+  const connectedUsers = await getUserData();
   const user = connectedUsers.find((user) => user.socketId === socket.id);
 
   if (user) {
     // remove user from room in server
+    const rooms = await getRoomData();
     const room = rooms.find((room) => room.id === user.roomId);
 
-    room.connectedUsers = room.connectedUsers.filter(
-      (user) => user.socketId !== socket.id
+    let remainingconnectedUsers = room.connectedUsers.filter(
+      (userProfile) => userProfile.socketId !== socket.id
     );
 
+    // console.log(remainingconnectedUsers)
     // leave socket io room
     socket.leave(user.roomId);
+    await removeUserData({socketId: socket.id})
+
+    let val = await updateRoomData({id: room.id},{connectedUsers : remainingconnectedUsers})
 
     // close the room if amount of the users which will stay in room will be 0
-    if (room.connectedUsers.length > 0) {
+    if (remainingconnectedUsers.length > 0) {
       // emit to all users which are still in the room that user disconnected
       io.to(room.id).emit("user-disconnected", { socketId: socket.id });
 
       // emit an event to rest of the users which left in the toom new connectedUsers in room
       io.to(room.id).emit("room-update", {
-        connectedUsers: room.connectedUsers,
+        connectedUsers: remainingconnectedUsers,
       });
     } else {
-      rooms = rooms.filter((r) => r.id !== room.id);
+      await removeRoomData({id: room.id});
     }
   }
 };
